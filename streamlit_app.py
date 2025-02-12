e2ab9cabd86dd2cff5c81fe74d02f9465f36d4d1
sreyakumar <121137643+sreyakumar@users.noreply.github.com>
2025-02-12 15:29:38 -0800

    feat: added langsmith accessible feedback

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,111 +1,127 @@
-import streamlit as st
 import asyncio
+import os
+import time
 import uuid
+import warnings
 
-# import sys
-# import os
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.runnables import RunnableConfig
+from langchain_core.tracers import LangChainTracer
+from langchain_core.tracers.context import collect_runs
+from langchain_core.tracers.run_collector import RunCollectorCallbackHandler
+from langgraph.checkpoint.memory import MemorySaver
+from langsmith import Client
+from streamlit_feedback import streamlit_feedback
 
 from metadata_chatbot.agents.async_workflow import async_workflow
 from metadata_chatbot.agents.mongo_db_agent import astream_input
 
-from langchain_core.messages import HumanMessage, AIMessage
-from langgraph.checkpoint.memory import MemorySaver
 
-from streamlit_feedback import streamlit_feedback
-from langsmith import Client
-from langchain_core.tracers.context import collect_runs
+warnings.filterwarnings("ignore")
 
-import logging
+load_dotenv()
 
-import uuid
-import warnings
-warnings.filterwarnings('ignore')
 
 @st.cache_resource
 def load_checkpointer():
     return MemorySaver()
 
-def setup_logging():
-    logger = logging.getLogger('feedback_logger')
-    if not logger.handlers:  # Prevent multiple handlers
-        logger.setLevel(logging.INFO)
-        file_handler = logging.FileHandler('response_feedback.log', mode='a')
-        formatter = logging.Formatter('%(asctime)s - %(message)s')
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-    return logger
 
-
-
-async def answer_generation(query: str, chat_history: list, config:dict, model):
+async def answer_generation(
+    query: str, chat_history: list, config: dict, model
+):
     inputs = {
-        "messages": chat_history, 
+        "messages": chat_history,
     }
     async for output in model.astream(inputs, config):
         for key, value in output.items():
             if key != "database_query":
-                yield value['messages'][0].content 
+                yield value["messages"][0].content
             else:
                 try:
                     query = str(chat_history) + query
-                    async for result in astream_input(query = query):
-                        response = result['type']
-                        if response == 'intermediate_steps':
-                            yield result['content']
-                        if response == 'agg_pipeline':
-                            yield "The MongoDB pipeline used to on the database is:" 
+                    async for result in astream_input(query=query):
+                        response = result["type"]
+                        if response == "intermediate_steps":
+                            yield result["content"]
+                        if response == "agg_pipeline":
+                            yield "The MongoDB pipeline used is:"
                             yield f"`{result['content']}`"
-                        if response == 'tool_response':
-                            yield "Retrieved output from MongoDB:" 
+                        if response == "tool_response":
+                            yield "Retrieved output from MongoDB:"
                             yield f"""```json
                                     {result['content']}
                                     ```"""
-                        if response == 'final_answer':
-                            yield result['content']
+                        if response == "final_answer":
+                            yield result["content"]
                 except Exception as e:
-                    yield f"An error has occured with the retrieval from DocDB: {e}. Try structuring your query another way."
+                    yield (
+                        "An error has occured with the retrieval from DocDB: "
+                        f"{e}. Try structuring your query another way."
+                    )
+
 
 def set_query(query):
     st.session_state.query = query
 
+
 async def main():
     st.title("GAMER: Generative Analysis of Metadata Retrieval")
 
-    client = Client()
+    langchain_api_key = os.getenv(
+        "LANGCHAIN_API_KEY"
+    )  # st.secrets.get("LANGCHAIN_API_KEY")
+    langchain_endpoint = os.getenv(
+        "LANGCHAIN_ENDPOINT"
+    )  # "https://api.smith.langchain.com"
+    project = os.getenv("LANGSMITH_PROJECT")
+    client = Client(api_url=langchain_endpoint, api_key=langchain_api_key)
 
-    if 'logger' not in st.session_state:
-        st.session_state.logger = setup_logging()
-
-    def log_feedback(response):
-        st.session_state.logger.info(response)
-
+    ls_tracer = LangChainTracer(project_name=project, client=client)
+    run_collector = RunCollectorCallbackHandler()
+    cfg = RunnableConfig()
+    cfg["callbacks"] = [ls_tracer, run_collector]
 
     if "thread_id" not in st.session_state:
-        st.session_state.thread_id = ''
+        st.session_state.thread_id = ""
     st.session_state.thread_id = str(uuid.uuid4())
-    
+
     checkpointer = load_checkpointer()
     model = async_workflow.compile(checkpointer=checkpointer)
 
-    if 'query' not in st.session_state:
-        st.session_state.query = ''
+    if "query" not in st.session_state:
+        st.session_state.query = ""
+
+    if "run_id" not in st.session_state:
+        st.session_state.query = None
 
     st.info(
-        "Ask a question about the AIND metadata! Please note that it will take a couple of seconds to generate an answer. Type a query to start or pick one of these suggestions:"
+        "Ask a question about the AIND metadata! "
+        "Please note that it will take a few seconds to generate an answer. "
+        "Type a query to start or pick one of these suggestions:"
     )
 
     examples = [
-        "What are the modalities that exist in the database? What are the least and most common ones?",
-        "What is the MongoDB query to find the injections used in SmartSPIM_675387_2023-05-23_23-05-56",
-        "Can you list all the procedures performed on 662616, including their start and end dates?"
+        (
+            "What are the modalities that exist in the database? "
+            "What are the least and most common ones?"
+        ),
+        (
+            "What is the MongoDB query to find the injections used in "
+            "SmartSPIM_675387_2023-05-23_23-05-56"
+        ),
+        (
+            "Can you list all the procedures performed on 662616, "
+            "including their start and end dates?"
+        ),
     ]
 
     columns = st.columns(len(examples))
     for i, column in enumerate(columns):
         with column:
-            st.button(examples[i], on_click = set_query, args=[examples[i]])
-
+            st.button(examples[i], on_click=set_query, args=[examples[i]])
 
     message = st.chat_message("assistant")
     message.write("Hello! How can I help you?")
@@ -113,11 +129,10 @@ async def main():
     user_query = st.chat_input("Message GAMER")
 
     if user_query:
-        st.session_state.query = user_query 
+        st.session_state.query = user_query
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
-
 
     for message in st.session_state.messages:
         if isinstance(message, HumanMessage):
@@ -128,33 +143,44 @@ async def main():
                 st.markdown(message.content)
 
     query = st.session_state.query
-    if query is not None and query != '':
+    if query is not None and query != "":
         st.session_state.messages.append(HumanMessage(query))
 
-        with st.chat_message("user"):
-            st.markdown(query)
+        # with st.chat_message("user"):
+        #     st.markdown(query)
+
+        st.chat_message("user").write(query)
 
         with st.chat_message("assistant"):
-            config = {"configurable": {"thread_id": st.session_state.thread_id}}
+            config = {
+                "configurable": {"thread_id": st.session_state.thread_id}
+            }
             prev = None
             generation = None
             chat_history = st.session_state.messages
             with collect_runs() as cb:
-                with st.status("Generating answer...", expanded = True) as status:
-                    async for result in answer_generation(query, chat_history, config, model):
-                        if prev != None:
+                with st.status(
+                    "Generating answer...", expanded=True
+                ) as status:
+                    async for result in answer_generation(
+                        query, chat_history, config, model
+                    ):
+                        if prev is not None:
                             st.markdown(prev)
                         prev = result
                         generation = prev
-                    status.update(label = "Answer generation successful.")
+                    status.update(label="Answer generation successful.")
+                    st.session_state.run_id = cb.traced_runs[-1].id
                     st.session_state.messages.append(AIMessage(generation))
-                    st.session_state.run_id = cb.traced_runs[0].id
-                final_response = st.empty()
-                log_feedback(f"Query: {query}")
-                log_feedback(f"Response: {generation}")
-                final_response.markdown(generation)
-        
+            final_response = st.empty()
+            final_response.markdown(generation)
                 
+
+    @st.cache_data(ttl="2h", show_spinner=False)
+    def get_run_url(run_id):
+        time.sleep(1)
+        return client.read_run(run_id).url
+
     if st.session_state.get("run_id"):
         run_id = st.session_state.run_id
         feedback = streamlit_feedback(
@@ -163,52 +189,33 @@ async def main():
             key=f"feedback_{run_id}",
         )
 
-        # Define score mappings for both "thumbs" and "faces" feedback systems
         score_mappings = {
             "faces": {"😀": 1, "🙂": 0.75, "😐": 0.5, "🙁": 0.25, "😞": 0},
         }
 
-        # Get the score mapping based on the selected feedback option
         scores = score_mappings["faces"]
 
         if feedback:
-            # Get the score from the selected feedback option's score mapping
             score = scores.get(feedback["score"])
-            
+
             if score is not None:
-                feedback = feedback.get("text")
-                log_feedback(f"SCORE: {score} | FEEDBACK: {feedback}")
-                st.toast("Feedback logged!", icon=":material/reviews:")
+                feedback_type_str = f"FACES: {feedback['score']}"
 
-                # Formulate feedback type string incorporating the feedback option
-                # and score value
-                #feedback_type_str = f"faces: {feedback['score']}"
+                feedback_record = client.create_feedback(
+                    run_id,
+                    feedback_type_str,
+                    score=score,
+                    comment=feedback.get("text"),
+                )
+                st.session_state.feedback = {
+                    "feedback_id": str(feedback_record.id),
+                    "score": score,
+                }
 
-                # # Record the feedback with the formulated feedback type string
-                # # and optional comment
-                # feedback_record = client.create_feedback(
-                #     run_id,
-                #     feedback_type_str,
-                #     score=score,
-                #     comment=feedback.get("text"),
-                # )
-                # st.session_state.feedback = {
-                #     "feedback_id": str(feedback_record.id),
-                #     "score": score,
-                # }
             else:
                 st.warning("Invalid feedback score.")
 
-    st.session_state.query = ''    
-
-    with open('response_feedback.log', 'r') as file:
-        log_contents = file.read()
-        st.download_button(
-            label="Download Feedback Log",
-            data=log_contents,
-            file_name="response_feedback.log",
-            mime="text/plain"
-        )
+    st.session_state.query = ""
 
 
 if __name__ == "__main__":
