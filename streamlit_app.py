d27903c83547632753437a244b1df2fd09ff096f
sreyakumar <121137643+sreyakumar@users.noreply.github.com>
2025-02-28 15:07:50 -0800

    feat: added sidebar (#9)
    * build: upgraded metadata-chatbot
    
    * perf: added streaming
    
    * build: upgrades
    
    * perf: fixed double answer generation issue
    
    * build: upgraded ver
    
    * updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import json
 import os
 import uuid
 import warnings
+from typing import AsyncGenerator
 
 import streamlit as st
 from dotenv import load_dotenv
@@ -27,7 +28,11 @@ def load_checkpointer():
     return MemorySaver()
 
 
-async def answer_generation(chat_history: list, config: dict, app, prev_generation):
+
+async def answer_generation(
+    chat_history: list, config: dict, app, prev_generation
+):
+
     """Streams GAMERS' node responses"""
     inputs = {
         "messages": chat_history,
@@ -35,7 +40,9 @@ async def answer_generation(chat_history: list, config: dict, app, prev_generati
 
     try:
 
-        async for result in stream_response(inputs, config, app, prev_generation):
+        async for result in stream_response(
+            inputs, config, app, prev_generation
+        ):
 
             yield result
 
@@ -44,6 +51,14 @@ async def answer_generation(chat_history: list, config: dict, app, prev_generati
             "An error has occured with the retrieval from DocDB: "
             f"{e}. Try structuring your query another way."
         )
+
+
+def to_sync_generator(async_gen: AsyncGenerator):
+    while True:
+        try:
+            yield asyncio.run(anext(async_gen))
+        except StopAsyncIteration:
+            break
 
 
 def set_query(query):
@@ -63,6 +78,9 @@ def initialize_session_state():
     if "model" not in st.session_state:
         checkpointer = load_checkpointer()
         st.session_state.model = workflow.compile(checkpointer=checkpointer)
+    if "generation" not in st.session_state:
+        st.session_state.generation = None
+
 
     if "generation" not in st.session_state:
         st.session_state.generation = None
@@ -71,27 +89,25 @@ async def typewriter_stream(result, container):
     full_response = ""
     text_content = result["content"]
 
-    if result['type'] == "tool_output":
+    if result["type"] == "tool_output":
+        try:
+            text_content = json.loads(text_content)
+        except:
+            text_content = json.loads(text_content[0])
+    # stream = text_content
 
-        text_content = json.loads(text_content)
+    if isinstance(text_content, str):
+        for word in text_content.split():
+            full_response += word + " "
+            container.write(full_response + " ")
+            await asyncio.sleep(0.05)
 
-    stream = text_content
-
-    if not isinstance(text_content, str):
-        stream = str(text_content)
-
-    for word in stream.split():
-        full_response += word + " "
-        container.write(full_response + " ")
-        await asyncio.sleep(
-            0.06
-        )
     container.write(text_content)
 
 
 async def main():
     """Main script to launch Streamlit UI"""
-    st.title("GAMER: Generative Analysis of Metadata Retrieval")
+    # st.title("GAMER: Generative Analysis of Metadata Retrieval")
 
     langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
     langchain_endpoint = os.getenv("LANGCHAIN_ENDPOINT")
@@ -105,11 +121,31 @@ async def main():
 
     initialize_session_state()
 
-    st.info(
+    with st.sidebar:
+
+        st.header("GAMER: Generative Analysis of Metadata Retrieval")
         "Ask a question about the AIND metadata! "
-        "Please note that it will take a few seconds to generate an answer. "
-        "Type a query to start or pick one of these suggestions:"
-    )
+        "Please note that it will take a few seconds to generate an answer."
+
+        with st.popover(
+            ":material/settings: Configurations", use_container_width=True
+        ):
+            data_routes = st.selectbox(
+                "Ask a question about the", options=("Metadata", "Data schema")
+            )
+
+            developer_mode = st.toggle("Developer mode")
+
+        (
+            "[Model architecture repository]"
+            "(https://github.com/AllenNeuralDynamics/metadata-chatbot)"
+        )
+        (
+            "[Streamlit app repository]"
+            "(https://github.com/sreyakumar/aind-GAMER-app)"
+        )
+
+    st.info("Type a query to start or pick one of these suggestions:")
 
     examples = [
         (
@@ -163,36 +199,59 @@ async def main():
             # prev = None
             generation = None
             message_stream = []
+            prev_generation = st.session_state.generation
 
             prev_generation = st.session_state.generation
 
 
             chat_history = st.session_state.messages
             with collect_runs() as cb:
-                with st.status(
-                    "Generating answer...", expanded=True
-                ) as status:
+
+                if developer_mode:
                     async for result in answer_generation(
 
-                        chat_history, config, st.session_state.model, prev_generation
+                        chat_history,
+                        config,
+                        st.session_state.model,
+                        prev_generation,
                     ):
-                        if result["type"] == "final_response":
+                        with st.spinner("Generating answer..."):
+                            if result["type"] == "final_response":
+                                generation = result
+                            else:
+                                temp_container = st.empty()
+                                await typewriter_stream(result, temp_container)
+                                message_stream.append(result)
 
-                            generation = result
-                        else:
-                            temp_container = st.empty()
-                            await typewriter_stream(result, temp_container)
+                else:
+                    with st.status(
+                        "Generating answer...", expanded=True
+                    ) as status:
+                        async for result in answer_generation(
+                            chat_history,
+                            config,
+                            st.session_state.model,
+                            prev_generation,
+                        ):
+                            if result["type"] == "final_response":
+                                generation = result
+                            else:
+                                temp_container = st.empty()
+                                await typewriter_stream(result, temp_container)
 
-                            message_stream.append(result)
+                                message_stream.append(result)
 
-                    status.update(label="Answer generation successful.")
+                        status.update(label="Answer generation successful.")
+
+                with st.spinner("Generating answer..."):
                     st.session_state.run_id = cb.traced_runs[-1].id
+                    st.session_state.messages.append(
+                        AIMessage(generation["content"])
+                    )
+                    st.session_state.generation = generation["content"]
+                    final_response = st.empty()
+                    await typewriter_stream(generation, final_response)
 
-                    st.session_state.messages.append(AIMessage(generation['content']))
-                    st.session_state.generation = generation['content']
-
-            final_response = st.empty()
-            await typewriter_stream(generation, final_response)
             # final_response.write(generation)
 
     if st.session_state.get("run_id"):
